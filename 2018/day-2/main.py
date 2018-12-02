028c33b2e14fa3ab0da0e4dbba3c278b89105b43
chadsten <chadsten.lowery@gmail.com>
2018-12-02 09:36:41 -0600

    code cleanup and comments

--- 2018/day-2/main.py
+++ 2018/day-2/main.py
@@ -1,24 +1,26 @@
-# get values for freq changes
+# load data
 import json
 from pprint import pprint
 
 with open('C:/Users/chadsten/source/repos/advent-of-code/2018/day-2/data.json') as data_file: 
 	data = json.load(data_file)
 
-print(data)
-
+# count both of these for our final checksum calculation
 doubles = 0
 triples = 0
 
 for s in data['strings']:
+	# create array of sorted letters, so we can do array position match/trim
 	s = list(s['value'])
 	s.sort()
+
+	# these are just used for short circuiting once one has been set
 	has_double = False
 	has_triple = False
 
 	while len(s) > 1 and (has_double == False or has_triple == False):
 
-		# find triples first
+		# find triple first
 		if len(s) > 2:
 			if s[0] == s[2] and has_triple == False:
 				triples = triples + 1
@@ -26,15 +28,15 @@ for s in data['strings']:
 				s = s[3:]
 				continue
 	
-		# find doubles
+		# find double
 		if s[0] == s[1] and has_double == False:
 			doubles = doubles + 1
 			has_double = True
 			s = s[2:]
 			continue
+
+		# drop letter without match
 		else:
 			s = s[1:]
 
 print("There are " + str(doubles) + " doubles and " + str(triples) + " triples, for a hash of " + str(doubles*triples) + ".")
-
-

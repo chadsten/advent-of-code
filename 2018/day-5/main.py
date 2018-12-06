6e1c1113435c8760845fe9604be3103a45f8d0fd
chadsten <chadsten.lowery@gmail.com>
2018-12-05 20:31:23 -0600

    d5p1 done

---
+++ 2018/day-5/main.py
@@ -0,0 +1,29 @@
+# load data
+import re
+with open('C:/Users/chadsten/source/repos/advent-of-code/2018/day-5/data.txt') as data_file: 
+	data = data_file.read().splitlines()
+
+alphabet = list()
+alphabet = ("Aa","Bb","Cc","Dd","Ee","Ff","Gg","Hh","Ii","Jj","Kk","Ll","Mm","Nn","Oo","Pp","Qq","Rr","Ss","Tt","Uu","Vv","Ww","Xx","Yy","Zz")  
+
+print(data[0])
+test = data[0]
+
+def reverse(s): 
+  str = "" 
+  for i in s: 
+    str = i + str
+  return str
+done = False
+
+while done == False:
+	before = len(test)
+	for letter in alphabet:
+		test = re.sub(letter, "", test)
+		test = re.sub(reverse(letter), "", test)
+	after = len(test)
+	if (after == before):
+		done = True
+
+print(test)
+print(len(test))
